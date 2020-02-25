35cec6bd51f326357d9555c3659d16e9eb2dd198
Jack Stankewicz <jack.stankewicz@gmail.com>
2020-02-25 14:22:15 -0600

    Moved significant code to a different file.

--- DupeFileFinder/Program.cs
+++ DupeFileFinder/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Security.Cryptography;
 
 namespace DupeFileFinder
 {
     class Program
     {
-        static readonly Dictionary<string, List<string>> hashStringToFilePaths = new Dictionary<string, List<string>>();
 
         static int Main(string[] args)
         {
@@ -20,40 +16,7 @@ namespace DupeFileFinder
 
             try
             {
-                var files = Directory.GetFiles(dirPath, "*", SearchOption.AllDirectories);
-
-                Console.WriteLine("Examining {0} files...", files.Length);
-                int count = 0;
-
-                foreach (var filePath in files)
-                {
-                    Console.WriteLine("Checking file {0}, {1} of {2}. {3}", filePath, ++count, files.Length, ((double)count / files.Length).ToString("P2"));
-
-                    string hashString = GetHashString(filePath);
-                    if (hashStringToFilePaths.TryGetValue(hashString, out List<string> filenames))
-                    {
-                        filenames.Add(filePath);
-                    }
-                    else
-                    {
-                        hashStringToFilePaths.Add(hashString, new List<string> { filePath });
-                    }
-                }
-
-                foreach (var hashString in hashStringToFilePaths.Keys)
-                {
-                    var filePaths = hashStringToFilePaths.GetValueOrDefault(hashString);
-                    if(filePaths.Count < 2)
-                    {
-                        continue;
-                    }
-
-                    Console.WriteLine("{0}:", hashString);
-                    foreach (var filePath in filePaths)
-                    {
-                        Console.WriteLine("\t{0} ", filePath);
-                    }
-                }
+                DupeFinderSingle.FindDupes(dirPath);
             }
             catch (Exception)
             {
@@ -62,14 +25,6 @@ namespace DupeFileFinder
             }
 
             return 0;
-        }
-
-        static string GetHashString(string filepath)
-        {
-            var md5 = MD5.Create();
-            var stream = File.OpenRead(filepath);
-            var hash = md5.ComputeHash(stream);
-            return BitConverter.ToString(hash).Replace("-", "").ToUpperInvariant();
         }
     }
 }
---
+++ DupeFileFinder/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DupeFileFinder
+{
+    class Utils
+    {
+        internal static string GetHashString(string filepath)
+        {
+            var md5 = MD5.Create();
+            var stream = File.OpenRead(filepath);
+            var hash = md5.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToUpperInvariant();
+        }
+    }
+}
